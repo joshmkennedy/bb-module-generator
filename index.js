e7173f04cdc47b1f4973bfbc7566b7124cb30fb4
Joshua Kennedy <josh@outthinkgroup.com>
2020-07-11 18:16:42 -0400

    added heruko port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { zipModule } = require("./zipModule.js");
 const { cleanUp } = require("./cleanup");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const settingsData = [
   {
